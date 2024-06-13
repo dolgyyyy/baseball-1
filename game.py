a6bbe67740e589033c23ca82dd264b55b8658976
heehoha <dolgyuk@gmail.com>
2024-06-13 14:55:09 +0900

    [refactoring] page 61

--- game.py
+++ game.py
@@ -13,10 +13,9 @@ class Game:
         else:
             strikes = 0
             for i in range(len(self.question)):
-                char = guessNumber[i]
-                index = self.question.find(char)
-                if index == i:
+                if self.question.find(guessNumber[i]) == i:
                     strikes += 1
+
             return GameResult(False, strikes, 0)
 
     def assert_illegal_value(self, guessNumber):
